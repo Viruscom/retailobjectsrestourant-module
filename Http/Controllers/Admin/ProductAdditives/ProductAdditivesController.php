4a51d6b5c111080b57ab68ecb063b75f1cd2330a
h.chobanov <hristiqn.chobanov@gmail.com>
2023-09-25 13:45:04 +0300

    sdfsdf

--- Http/Controllers/Admin/ProductAdditives/ProductAdditivesController.php
+++ Http/Controllers/Admin/ProductAdditives/ProductAdditivesController.php
@@ -1,99 +1,101 @@
 <?php
 
-namespace Modules\RetailObjectsRestourant\Http\Controllers\Admin\ProductAdditives;
+    namespace Modules\RetailObjectsRestourant\Http\Controllers\Admin\ProductAdditives;
 
-use App\Actions\CommonControllerAction;
-use App\Helpers\CacheKeysHelper;
-use App\Helpers\LanguageHelper;
-use App\Helpers\MainHelper;
-use App\Helpers\WebsiteHelper;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Modules\RetailObjectsRestourant\Http\Requests\ProductAdditiveStoreRequest;
-use Modules\RetailObjectsRestourant\Http\Requests\ProductAdditiveUpdateRequest;
-use Modules\RetailObjectsRestourant\Models\ProductAdditive;
-use Modules\RetailObjectsRestourant\Models\ProductAdditiveTranslation;
+    use App\Actions\CommonControllerAction;
+    use App\Helpers\CacheKeysHelper;
+    use App\Helpers\LanguageHelper;
+    use App\Helpers\MainHelper;
+    use App\Helpers\WebsiteHelper;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\RedirectResponse;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Cache;
+    use Modules\RetailObjectsRestourant\Http\Requests\ProductAdditiveStoreRequest;
+    use Modules\RetailObjectsRestourant\Http\Requests\ProductAdditiveUpdateRequest;
+    use Modules\RetailObjectsRestourant\Models\ProductAdditive;
+    use Modules\RetailObjectsRestourant\Models\ProductAdditiveTranslation;
 
-class ProductAdditivesController extends Controller
-{
-    public function index()
+    class ProductAdditivesController extends Controller
     {
-        if (is_null(Cache::get(CacheKeysHelper::$SHOP_PRODUCT_ADDITIVES))) {
-            ProductAdditive::cacheUpdate();
-        }
-
-        return view('retailobjectsrestourant::admin.product_additives.index', ['characteristics' => Cache::get(CacheKeysHelper::$SHOP_PRODUCT_ADDITIVES)]);
-    }
-    public function store(ProductAdditiveStoreRequest $request, CommonControllerAction $action)
-    {
-        $productAdditive = $action->doSimpleCreate(ProductAdditive::class, $request);
-        ProductAdditive::cacheUpdate();
-        $productAdditive->storeAndAddNew($request);
-
-        return redirect()->route('admin.product.additives.index')->with('success-message', trans('admin.common.successful_create'));
-    }
-    public function edit($id)
-    {
-        $productAdditive = ProductAdditive::where('id', $id)->with('translations')->first();
-        WebsiteHelper::redirectBackIfNull($productAdditive);
-
-        if (is_null(Cache::get(CacheKeysHelper::$SHOP_PRODUCT_ADDITIVES))) {
-            ProductAdditive::cacheUpdate();
-        }
-
-        return view('retailobjectsrestourant::admin.product_additives.edit', [
-            'productCharacteristic' => $productAdditive,
-            'languages'             => LanguageHelper::getActiveLanguages(),
-        ]);
-    }
-    public function deleteMultiple(Request $request, CommonControllerAction $action): RedirectResponse
-    {
-        if (!is_null($request->ids[0])) {
-            $ids = array_map('intval', explode(',', $request->ids[0]));
-            foreach ($ids as $id) {
-                $model = ProductAdditive::find($id);
-                if (is_null($model)) {
-                    continue;
-                }
-
-                $model->delete();
+        public function index()
+        {
+            if (is_null(Cache::get(CacheKeysHelper::$SHOP_PRODUCT_ADDITIVES))) {
+                ProductAdditive::cacheUpdate();
             }
 
+            return view('retailobjectsrestourant::admin.product_additives.index', ['characteristics' => Cache::get(CacheKeysHelper::$SHOP_PRODUCT_ADDITIVES)]);
+        }
+        public function store(ProductAdditiveStoreRequest $request, CommonControllerAction $action)
+        {
+            $productAdditive = $action->doSimpleCreate(ProductAdditive::class, $request);
+            ProductAdditive::cacheUpdate();
+            if ($request->has('submitaddnew')) {
+                return redirect()->back()->with('success-message', 'admin.common.successful_create');
+            }
+
+            return redirect()->route('admin.product.additives.index')->with('success-message', trans('admin.common.successful_create'));
+        }
+        public function edit($id)
+        {
+            $productAdditive = ProductAdditive::where('id', $id)->with('translations')->first();
+            WebsiteHelper::redirectBackIfNull($productAdditive);
+
+            if (is_null(Cache::get(CacheKeysHelper::$SHOP_PRODUCT_ADDITIVES))) {
+                ProductAdditive::cacheUpdate();
+            }
+
+            return view('retailobjectsrestourant::admin.product_additives.edit', [
+                'productCharacteristic' => $productAdditive,
+                'languages'             => LanguageHelper::getActiveLanguages(),
+            ]);
+        }
+        public function deleteMultiple(Request $request, CommonControllerAction $action): RedirectResponse
+        {
+            if (!is_null($request->ids[0])) {
+                $ids = array_map('intval', explode(',', $request->ids[0]));
+                foreach ($ids as $id) {
+                    $model = ProductAdditive::find($id);
+                    if (is_null($model)) {
+                        continue;
+                    }
+
+                    $model->delete();
+                }
+
+                ProductAdditive::cacheUpdate();
+
+                return redirect()->back()->with('success-message', 'admin.common.successful_delete');
+            }
+
+            return redirect()->back()->withErrors(['admin.common.no_checked_checkboxes']);
+        }
+        public function delete($id, CommonControllerAction $action): RedirectResponse
+        {
+            $productAdditive = ProductAdditive::where('id', $id)->first();
+            MainHelper::goBackIfNull($productAdditive);
+
+            $productAdditive->delete();
             ProductAdditive::cacheUpdate();
 
             return redirect()->back()->with('success-message', 'admin.common.successful_delete');
         }
+        public function update($id, ProductAdditiveUpdateRequest $request, CommonControllerAction $action): RedirectResponse
+        {
+            $productAdditive = ProductAdditive::find($id);
+            if (is_null($productAdditive)) {
+                return redirect()->back()->withInput()->withErrors(['admin.common.record_not_found']);
+            }
 
-        return redirect()->back()->withErrors(['admin.common.no_checked_checkboxes']);
-    }
-    public function delete($id, CommonControllerAction $action): RedirectResponse
-    {
-        $productAdditive = ProductAdditive::where('id', $id)->first();
-        MainHelper::goBackIfNull($productAdditive);
+            $action->doSimpleUpdate(ProductAdditive::class, ProductAdditiveTranslation::class, $productAdditive, $request);
+            ProductAdditive::cacheUpdate();
 
-        $productAdditive->delete();
-        ProductAdditive::cacheUpdate();
-
-        return redirect()->back()->with('success-message', 'admin.common.successful_delete');
-    }
-    public function update($id, ProductAdditiveUpdateRequest $request, CommonControllerAction $action): RedirectResponse
-    {
-        $productAdditive = ProductAdditive::find($id);
-        if (is_null($productAdditive)) {
-            return redirect()->back()->withInput()->withErrors(['admin.common.record_not_found']);
+            return redirect()->route('admin.product.additives.index')->with('success-message', 'admin.common.successful_edit');
         }
-
-        $action->doSimpleUpdate(ProductAdditive::class, ProductAdditiveTranslation::class, $productAdditive, $request);
-        ProductAdditive::cacheUpdate();
-
-        return redirect()->route('admin.product.additives.index')->with('success-message', 'admin.common.successful_edit');
+        public function create()
+        {
+            return view('retailobjectsrestourant::admin.product_additives.create', [
+                'languages' => LanguageHelper::getActiveLanguages()
+            ]);
+        }
     }
-    public function create()
-    {
-        return view('retailobjectsrestourant::admin.product_additives.create', [
-            'languages' => LanguageHelper::getActiveLanguages()
-        ]);
-    }
-}
--- Resources/lang/bg/admin.php
+++ Resources/lang/bg/admin.php
@@ -1,46 +1,51 @@
 <?php
 
-    return [
-        'title'      => 'Име на обект',
-        'email'      => 'E-mail',
-        'address'    => 'Адрес',
-        'phone'      => 'Телефон',
-        'map_iframe' => 'Сорс към Google карта',
-
-        'restaurants'               => [
-            'index'                 => 'Ресторанти',
-            'create'                => 'Добавяне на ресторант',
-            'edit'                  => 'Редактиране на ресторант',
-            'restourant'            => 'Ресторант',
-            'email'                 => 'E-mail',
-            'working_time'          => 'Работно време',
-            'working_time_mon_fri'  => 'понеделник-петък',
-            'working_time_saturday' => 'събота',
-            'working_time_sunday'   => 'неделя',
-        ],
-        'retail_object'             => 'Обекти',
-        'delivery_zones'            => 'Зони на доставка',
-        'settings'                  => [
-            'index'                    => 'Ресторанти настройки',
-            'google_maps_api_key'      => 'Google Maps API KEY',
-            'google_geocoding_api_key' => 'Google Geocoding API KEY'
-        ],
-        'restaurants_delivery_zone' => [
-            'index' => 'Зона на доставки',
-        ],
-        'restaurants_working_time'  => [
-            'index' => 'Работно време',
-        ],
-        'product_additives'         => [
-            'index'            => 'Добавки',
-            'create'           => 'Добавяне на Добавка',
-            'edit'             => 'Редактиране на добавка',
-            'no_records_found' => 'Няма намерени добавки',
-            'title'            => 'Заглавие',
-            'price'            => 'Цена',
-        ],
-        'restaurant_priorities'     => [
-            'workload_priority_status_required' => 'Приоритетът на натовареност за ресторанта е задължителен',
-            'workload_priority_status_in'       => 'Приоритетът на натовареност за ресторанта не е валиден',
-        ],
-    ];
+return array (
+  'title' => 'Име на обект',
+  'email' => 'E-mail',
+  'address' => 'Адрес',
+  'phone' => 'Телефон',
+  'map_iframe' => 'Сорс към Google карта',
+  'restaurants' => 
+  array (
+    'index' => 'Ресторанти',
+    'create' => 'Добавяне на ресторант',
+    'edit' => 'Редактиране на ресторант',
+    'restourant' => 'Ресторант',
+    'email' => 'E-mail',
+    'working_time' => 'Работно време',
+    'working_time_mon_fri' => 'понеделник-петък',
+    'working_time_saturday' => 'събота',
+    'working_time_sunday' => 'неделя',
+  ),
+  'retail_object' => 'Обекти',
+  'delivery_zones' => 'Зони на доставка',
+  'settings' => 
+  array (
+    'index' => 'Ресторанти настройки',
+    'google_maps_api_key' => 'Google Maps API KEY',
+    'google_geocoding_api_key' => 'Google Geocoding API KEY',
+  ),
+  'restaurants_delivery_zone' => 
+  array (
+    'index' => 'Зона на доставки',
+  ),
+  'restaurants_working_time' => 
+  array (
+    'index' => 'Работно време',
+  ),
+  'product_additives' => 
+  array (
+    'index' => 'Добавки',
+    'create' => 'Добавяне на Добавка',
+    'edit' => 'Редактиране на добавка',
+    'no_records_found' => 'Няма намерени добавки',
+    'title' => 'Заглавие',
+    'price' => 'Цена',
+  ),
+  'restaurant_priorities' => 
+  array (
+    'workload_priority_status_required' => 'Приоритетът на натовареност за ресторанта е задължителен',
+    'workload_priority_status_in' => 'Приоритетът на натовареност за ресторанта не е валиден',
+  ),
+);
